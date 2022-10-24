be9647362ff21d085ee543d99831744bdd33c98d
Amaya Lucas <amayajlucas@gmail.com>
2022-10-24 09:36:47 -0400

    pages started, nav bar and links started

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,21 @@
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Home from './pages/Home'
+import About from './pages/About'
 
 const App = () => {
-  return <div>let's go architecture!</div>
+  return (
+    <div>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </main>
+    </div>
+  )
 }
 
 export default App
---
+++ client/src/pages/Home.js
@@ -0,0 +1,13 @@
+import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+
+const Home = () => {
+  return (
+    <div>
+      <h1>HOME</h1>
+    </div>
+  )
+}
+
+export default Home
