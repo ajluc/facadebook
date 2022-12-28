1de23afca10e96ddeefed4a9eb6425611a46b02d
Amaya Lucas <amayajlucas@gmail.com>
2022-12-27 23:05:08 -0700

    oops forgot import usestate

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
+import { useState } from 'react'
 import Header from './components/Header'
 import Home from './pages/Home'
 import About from './pages/About'
 import ViewBuildingsOfStyle from './pages/ViewBuildingsOfStyle'
 import Building from './pages/Building'
-import AddBldg from './pages/AddBldg'
+// import AddBldg from './pages/AddBldg'
 import AddStyleForm from './components/AddArchStyle'
 import AddBuildingForm from './components/AddBuilding'
 
